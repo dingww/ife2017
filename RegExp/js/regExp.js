fb9d4ade316b993baf299dd4d23bee445b7c4c9b
dingww <1067061570@qq.com>
2017-02-25 18:37:01 +0800

    修改界面

--- RegExp/js/regExp.js
+++ RegExp/js/regExp.js
@@ -1,34 +1,93 @@
 window.onload = function() {
-	var oInput1 = document.getElementById('input1');
-	var oInput2 = document.getElementById('input2');
-	var oBtn1 = document.getElementById('btn1');
-	var oBtn2 = document.getElementById('btn2');
-	// 验证手机号码
-	oBtn1.onclick = function() {
-		var isMatch1 = document.getElementById('isMatch1');
-		var text1 = oInput1.value;
-		// var reg = /^1[358][0-9]{9}$/; //只能匹配不带'-'格式的手机号码
-		var reg = /^1[358]\d[-]?\d{8}$/; //可匹配带'-'和不带'-'格式的11为手机号码
-		var num = text1.match(reg);
-		console.log(num);
-		if (num === null) {
-			isMatch1.innerHTML = 'false';
-		} else {
-			isMatch1.innerHTML = 'true';
+	var oBtn = document.getElementById('btn');
+	var oDown = document.getElementById('down');
+	var oUl = document.getElementsByTagName('ul')[0];
+	var oLi = document.getElementsByTagName('li');
+	var oReg = document.getElementById('inputReg');
+	var oText = document.getElementById('inputTxt');
+	var oResult = document.getElementById('result');
+
+	//可匹配带“+86”和不带“+86”的手机号，手机号码段查询结果为： 
+	// 150、151、152、153、155、156、157、158、159 九个 
+	// 130、131、132、133、134、135、136、137、138、139 十个 
+	// 180、182、185、186、187、188、189 七个
+	// 13、15、18共30个号段，154、181、183、184暂时没有，加上147共27个。
+	var numReg = /(^1|\s1|^\+861|\s\+861)(3\d|47|5[0-35-9]|8[025-9])\d{8}|^\+861(3\d|47|5[0-35-9]|8[025-9])\d{8}/g;
+
+	// 测试是否有效相邻单词的正则表达式
+	var wordReg = /\b([A-Za-z]+(-[A-Za-z]+)*)\s\1(\s|$)/g;
+
+	var reg = [numReg, wordReg, ''];
+
+	// 显示正则表达式选项菜单
+	oDown.onclick = function(event) {
+		oUl.style.display = 'block';
+		for (var i = 0; i < oLi.length; i++) {
+			oLi[i].index = i;
+			oReg.value = '';
+			oLi[i].onclick = function() {
+				oReg.value = reg[this.index];
+			}
+		}
+	}
+	document.onclick = function(e) {
+		var oTarget = e.target || e.srcElement;
+		if (oTarget.id !== 'down') {
+			oUl.style.display = 'none';
 		}
 	}
 
-	// 验证是否有相邻重复的单词
-	oBtn2.onclick = function() {
-		var isMatch2 = document.getElementById('isMatch2');
-		var text2 = oInput2.value;
-		var reg = /\b(\w+)\s\1\s\b|\b(\w+)\s\1\b/;
-		var str = text2.match(reg);
-		console.log(str);
-		if (str === null) {
-			isMatch2.innerHTML = 'false';
+	// 验证匹配项
+	oBtn.onclick = function() {
+		var testTxt = oText.value;
+		var regExp = oReg.value;
+		if (regExp == reg[0]) {
+			testPhonenumber(testTxt, regExp, oResult);
+			// alert(regExp);
+		} else if (regExp == reg[1]) {
+			testReword(testTxt, regExp, oResult);
+			// alert(regExp);
 		} else {
-			isMatch2.innerHTML = 'true';
+			// alert(regExp);
+			testSelfDef(testTxt, regExp, oResult);
+
 		}
+	}
+
+	// 匹配手机号码函数
+	function testPhonenumber(number, reg, node) {
+		var Reg = eval(reg);
+		var matchN = number.match(Reg);
+		var item = '';
+		if (matchN) {
+			item += '<p>是否有匹配：true</p><p>匹配的手机号码是：' + matchN + '</p>';
+		} else {
+			item += '<p>是否有匹配：false</p><p>提示：请输入手机号码。</p>';
+		}
+		node.innerHTML = item;
+	}
+	// 匹配相邻单词函数
+	function testReword(word, reg, node) {
+		var Reg = eval(reg);
+		var matchW = word.match(Reg);
+		var item = '';
+		if (matchW) {
+			item += '<p>是否有匹配：true</p><p>匹配的相邻单词是：' + matchW + '</p>';
+		} else {
+			item += '<p>是否有匹配：false</p><p>提示：没有找到相邻的单词。</p>'
+		}
+		node.innerHTML = item;
+	}
+	// 匹配自定义项函数
+	function testSelfDef(text, reg, node) {
+		var Reg = eval(reg);
+		var matchT = text.match(Reg);
+		var item = '';
+		if (matchT) {
+			item += '<p>是否有匹配：true</p><p>您的匹配项是：' + matchT + '</p>';
+		} else {
+			item += '<p>是否有匹配：false</p><p>提示：没有找到匹配项。</p>'
+		}
+		node.innerHTML = item;
 	}
 }
