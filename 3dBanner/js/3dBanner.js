1b09badf66e9c4e831b85bdab546daa1e1c14f10
dingww <1067061570@qq.com>
2017-03-23 15:32:56 +0800

    optimize1

--- 3dBanner/js/3dBanner.js
+++ 3dBanner/js/3dBanner.js
@@ -6,31 +6,39 @@ var oRight = document.getElementById('right');
 var timer;
 var len = oImg.length;
 var flag = 0;
-oWrap.style.cssText = 'transform:translate3d( 0, 0, -1000px ) rotateY(0deg)';
 
+// 初始状态
+slide(0);
+Timer(flag);
+// 左右按钮的显示与隐藏
 oStage.onmouseover = function() {
 	clearInterval(timer);
 	oLeft.style.visibility = 'visible';
 	oRight.style.visibility = 'visible';
 }
 oStage.onmouseout = function() {
-	oLeft.style.visibility = 'hidden';
-	oRight.style.visibility = 'hidden';
-	timer = setInterval(function() {
-		flag++;
-		oWrap.style.cssText = 'transform:translate3d( 0, 0, -1000px ) rotateY(' + (flag * 36) + 'deg)';
-	}, 2000);
-}
+		oLeft.style.visibility = 'hidden';
+		oRight.style.visibility = 'hidden';
+		Timer(flag);
+	}
+	// 点击左按钮向左滑动
 oLeft.onclick = function() {
-	flag--;
-	oWrap.style.cssText = 'transform:translate3d( 0, 0, -1000px ) rotateY(' + (flag * 36) + 'deg)';
-}
+		flag--;
+		slide(flag);
+	}
+	// 点击右按钮向右滑动
 oRight.onclick = function() {
-	flag++;
-	oWrap.style.cssText = 'transform:translate3d( 0, 0, -1000px ) rotateY(' + (flag * 36) + 'deg)';
+		flag++;
+		slide(flag);
+	}
+	// 定义滑动函数
+function slide(n) {
+	oWrap.style.cssText = 'transform:translate3d( 0, 0, -1000px ) rotateY(' + n * 36 + 'deg)';
 }
-
-timer = setInterval(function() {
-	flag++;
-	oWrap.style.cssText = 'transform:translate3d( 0, 0, -1000px ) rotateY(' + (flag * 36) + 'deg)';
-}, 2000)
+// 定义定时器，自动向右滑动展示
+function Timer(n) {
+	timer = setInterval(function() {
+		n++;
+		slide(n);
+	}, 2000)
+}
